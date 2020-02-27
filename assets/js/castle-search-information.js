6b305ccc9fae136e6d302987cb9989e84e62a1d2
CharOConnell <charlotteoconnell@virginmedia.com>
2020-02-27 11:10:02 +0000

    Map placeholder whilst sorting out the search function. Added the enter key to syphon the ().then() function inside js. Added a loading gif in the search js

--- assets/js/castle-search-information.js
+++ assets/js/castle-search-information.js
@@ -1,20 +1,28 @@
 function fetchChateauInformation(event) {
 
-    var castleName = $("chateau-search-name").val();
+    var castleName = $("#chateau-search-name").val();
+    
     // if we don't have any castles selected!
     if (!castleName) {
         $("#data").html(`<p>No castles selected! Please search or select one from the map above.</p>`);
         return;
-    } 
-    // IT IS SHOWING UP WITH THIS WHEN YOU ARE TYPING WHICH IS NOT RIGHT!
-    
-    // have a loader - not working :(
-    // $("#data").html(`<div id="loader> Loading ... <i class="fa fa-fort-awesome fa-xs fa-spin"></i> </div>`);
+    }
 
-    $.when(
-        // we have a name entered - maybe when the enter key is pushed?
-    ).then (
-        // function to display the one map with the one marker, zoomed in, with all the data underneath!
-        // need to make sure that it loads nearby in case of just a location, but set radius low!
-    )
+    // have a loader to let the user know what is going on
+    $("#data").html(`<div id="loader"><i class="fa fa-fort-awesome"></i> <i class="fa fa-map-o"></i> <i class="fa fa-map-marker"></i> <i class="fa fa-globe"></i> <i class="fa fa-crosshairs"></i> <i class="fa fa-flag"></i> <i class="fa fa-location-arrow"></i>  . . .  loading castle data . . . <i class="fa fa-cog fa-spin"></i> <i class="fa fa-fort-awesome fa-spin"></i> </div>`);
+
+    $(document).keypress(function (event) {
+        let keycode = (event.keyCode ? event.keyCode : event.which);
+
+        if (keycode == 13) { // if enter is pressed - we want to search!
+            console.log('you have pressed enter!')
+            $.when(
+                // we have a name entered - get a response from nearby search
+            ).then(
+                // function to display the one map with the one marker, zoomed in, with all the data underneath!
+                // need to make sure that it loads nearby in case of just a location, but set radius low!
+            )
+        }
+    });
+
 }
