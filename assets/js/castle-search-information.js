a886f912b770989cd0ea4c44532244dbdcfd5ccb
CharOConnell <charlotteoconnell@virginmedia.com>
2020-03-16 15:53:04 +0000

    Changed method of searching to a button search, taking out over complicated JS functionality

--- assets/js/castle-search-information.js
+++ assets/js/castle-search-information.js
@@ -15,22 +15,4 @@ function fetchChateauInformation(event) {
     $("#castle-data-name").html(``); // empty the data that might already be there from clicking on the map
     $("#castle-data").html(`<div id="loader"><i class="fa fa-fort-awesome"></i> <i class="fa fa-map-marker"></i> . . .  Loading castle data . . . <i class="fa fa-globe fa-spin"></i> </div>`);
 
-    /*
-    $(document).keypress(function (event) {
-        let keycode = (event.keyCode ? event.keyCode : event.which);
-        if (keycode === 13) {// enter is pressed
-            $.when(
-                console.log(castleName)
-            ).then(
-                console.log('testing that enter has been pressed')
-                // function to display the one map with the one marker, zoomed in, with all the data underneath!
-                // need to make sure that it loads nearby in case of just a location, but set radius low!
-            )
-        }
-    }
-    );*/
-
-    // can just do it with a search box!
-    
-
 }
