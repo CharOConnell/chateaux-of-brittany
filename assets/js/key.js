343e6be32aadc65b7a9a47857829b5a63de2a736
CharOConnell <charlotteoconnell@virginmedia.com>
2020-03-31 12:12:39 +0000

    Starting to use .env file for API key, javascript trying to access it to take it out of the html

---
+++ assets/js/key.js
@@ -0,0 +1,14 @@
+/*document.addEventListener('DOMContentLoaded', function () {
+  if (document.querySelectorAll('#map').length > 0)
+  {
+    var js_file = document.createElement('script');
+    js_file.type = 'text/javascript';
+    js_file.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&libraries=places&key=' + "letsdothis";
+    console.log(js_file)
+    // document.getElementsByTagName('script')[15].appendChild(js_file);
+  }
+});*/
+
+
+
+console.log(process.env)
