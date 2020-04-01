3afdb19c627c27a946f4e538a2e8b1df21fb609b
CharOConnell <charlotteoconnell@virginmedia.com>
2020-04-01 10:11:46 +0000

    Added gitignore file

--- assets/js/mykey.js
+++ assets/js/mykey.js
@@ -1,2 +1,2 @@
-var mykey = "asecret";
+var mykey = "AIzaSyAyrCzti1IFJ1Q2TcZnMqha4OmdS5nyZkE";
 document.getElementById("testingkey").src = "https://maps.googleapis.com/maps/api/js?callback=initMap&libraries=places&key=" + mykey;
