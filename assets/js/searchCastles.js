017ce32473478b2db91a0ed1fee42b2724acca3c
CharOConnell <charlotteoconnell@virginmedia.com>
2020-04-07 09:23:43 +0000

    Added no results found to input box when no results found. Restricted searches to be within Brittany as this had been a bug

--- assets/js/searchCastles.js
+++ assets/js/searchCastles.js
@@ -6,7 +6,7 @@ function searchCastles() {
     if (castleLength == 0) {
         emptyData() // empty any data already existing
         $("#castle-data-name").html(`<h4>No Results Found.</h4>`); // display no results found
-        $("#loading-data").html(`<p>Please enter another name and try again!</p>`);
+        $("#loading-data").html(`<p>Please enter another name and try again! Initial entries loaded.</p>`);
         initialMap() // load initial map
 
     } else if ((castleLength > 0) && (castleLength <= 2)) {
@@ -21,7 +21,7 @@ function searchCastles() {
 
 function emptyData() {
     // empty the data that might already exist in the interactive results boxes
-    $("#castle-data-name").html(``); 
+    $("#castle-data-name").html(``);
     $("#castle-data").html(``);
     $('#castle-photos').html(``);
     $('#loading-data').html(``);
@@ -33,13 +33,13 @@ function initialMap() {
         center: { lat: 48.000, lng: -3.000 },
         zoom: 7.5
     });
-
+    
     // search for chateau within a radius of the centre of Brittany
     var request = {
         location: { lat: 48.000, lng: -3.000 },
-        radius: 80000,
+        radius: '99999',
         keyword: 'chateau',
-        fields: ['name', 'geometry', 'photos', 'rating', 'opening_hours']
+        fields: ['name', 'geometry', 'photos', 'rating']
     };
 
     service = new google.maps.places.PlacesService(map);
@@ -56,19 +56,20 @@ function initialMap() {
 
 function newCastleSearch() {
     // search for anything typed into the search box accompagnied with "chateau" to force the results
+    var brittanyBounds = {north: 49.7, south: 46.8, west: -5.2, east: -0.11};
     var request = {
-        location: { lat: 48.000, lng: -3.000 },
-        radius: 20000,
-        query: ['chateau', castleName],
+        bounds: brittanyBounds,
+        keyword: ['chateau', castleName],
         fields: ['name', 'geometry', 'photos', 'rating']
     };
 
     service = new google.maps.places.PlacesService(map);
-    service.textSearch(request, callback);
+    service.nearbySearch(request, callback);
 
     function callback(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) { 
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
             // refresh map to remove old markers
+
             map = new google.maps.Map(document.getElementById('map'), {
                 center: { lat: 48.000, lng: -3.000 },
                 zoom: 7.5
@@ -77,11 +78,20 @@ function newCastleSearch() {
                 var place = results[i];
                 createMarker(results[i]);
             }
-            if (results.length == 0) { 
+            if (results.length == 0) {
                 // if no results found, display no results to user
                 $("#castle-data-name").html(`<h4>No Results Found.</h4>`);
                 $("#castle-data").html(`<p>Please enter another name and try again!</p>`);
             }
+        } else {
+            // display clear map
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: { lat: 48.000, lng: -3.000 },
+                zoom: 7.5
+            });
+            // no results are found for that string
+            $("#castle-data-name").html(`<h4>No Results Found.</h4>`);
+            $("#loading-data").html(`<p>No Results Found! Please enter another name and try again!</p>`);
         }
     }
 }
